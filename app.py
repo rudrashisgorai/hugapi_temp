478880cdaf6e1fbeff6e05469f7d8a6813bdd1cf
rudrashis gorai <rudrashis.gorai@gmail.com>
2021-01-06 00:21:17 +0530

    Edit in app

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ def temp(city: hug.types.text ):
         call = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}"
         data = requests.get(call)
         city_data = data.json()
-        return { "City":{city_data['name']},
-                "Temperature" :{round(city_data['main']['temp']-273)}
+        return { "City":city_data['name'],
+                "Temperature" :round(city_data['main']['temp']-273)
         }
     except:
         return {
